24a62692963e02e8a3ff89993dea57d912e528f8
Tony S <59147271+TheJokerDev@users.noreply.github.com>
2024-01-25 18:05:56 -0600

    Upload

---
+++ src/main/java/me/j0keer/xtroll/XTroll.java
@@ -0,0 +1,17 @@
+package me.j0keer.xtroll;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XTroll extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
